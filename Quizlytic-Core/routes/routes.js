df659ec77d3166db7bb2ea11c064521420ecd66f
Thiago Mazzaroli <thiagomazzaroli63@gmail.com>
2025-01-21 22:13:13 -0300

    sei la

---
+++ Quizlytic-Core/routes/routes.js
@@ -0,0 +1,13 @@
+import express from "express"
+
+const router = express.Router()
+
+router.post('/criar')
+
+router.delete('/delete')
+
+router.get('/findAll')
+
+router.get('/findById')
+
+export default router
--- Quizlytic-Core/server.js
+++ Quizlytic-Core/server.js
@@ -1,5 +1,21 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
+
+const testConnection = async () => {
+    try {
+        // Tenta uma query básica no banco
+        await prisma.$connect();
+        console.log("Conexão com o banco de dados bem-sucedida!");
+    } catch (error) {
+        console.error("Erro ao conectar com o banco de dados:", error.message);
+    } finally {
+        await prisma.$disconnect();
+    }
+};
+
+testConnection();
 
 const app = express();
 
