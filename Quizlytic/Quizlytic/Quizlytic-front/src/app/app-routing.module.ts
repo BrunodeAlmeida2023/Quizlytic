207a14dbea5f47ec2f374e7ca19891f8a125a16f
Bruno de Almeida <brunoalmeida0506@gmail.com>
2025-01-21 22:18:41 -0300

    .

--- Quizlytic/Quizlytic/Quizlytic-front/src/app/app-routing.module.ts
+++ Quizlytic/Quizlytic/Quizlytic-front/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Page/login/login.component'; // Substitua pelo nome correto do componente
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redireciona para a tela de login
+  { path: 'login', component: LoginComponent }, // Rota para o componente de login
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)], // Configuração principal das rotas
+  exports: [RouterModule], // Exporta o RouterModule para uso em outros módulos
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Quizlytic/Quizlytic/Quizlytic-front/src/app/app.module.ts
+++ Quizlytic/Quizlytic/Quizlytic-front/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms'; // Import necessário para usar [(ngModel)]
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Page/login/login.component'; // Import do componente de login
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, // Necessário para rodar o Angular no navegador
+    FormsModule, // Necessário para trabalhar com formulários e [(ngModel)]
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent], // Componente raiz do aplicativo
 })
-export class AppModule { }
+export class AppModule {}
